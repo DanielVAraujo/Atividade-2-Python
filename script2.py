3a2b76e2e93e18eabe8ff7cc4059a752bf2067bb
DanielVAraujo <160507946+DanielVAraujo@users.noreply.github.com>
2024-09-03 10:27:30 -0300

    Add files via upload

---
+++ script2.py
@@ -0,0 +1,37 @@
+#Atividade 2 - Python ft. BoraBill
+
+"""
+Programador: Daniel Araújo
+Programa Para Testar Várias Funções em Python
+
+"""
+
+if 10 > 1:
+    print("A Condição é Verdadeira")
+else:
+    print ("A Condição é Falsa")
+
+x = str (3) # Variavel Tipo Texto (String)
+y = int(3) # Variavel Tipo Inteiro (Int)
+z = float (3) # Varaivel de Tipo Real Flutuante (Float)
+
+print (x)
+print (y)
+print (z)
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+frutas = ["banana","maçã","mamão"]
+a,b,c = frutas
+print(a)
+print(b)
+print(c)
+
+print (a, b, c)
+print (a + b + c)
+
+
+
+print ("Fim do Programa!")
